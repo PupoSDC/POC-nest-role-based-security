1a6390fe5a21bc7d3d21c7ce573c2178e0c0e888
Pedro Pupo Sa da Costa <pedro@hublo.com>
2021-12-27 16:36:03 +0000

    test: add test cases for scenarios

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -5,6 +5,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return "this is working";
+    return "Hello World!";
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,107 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('/institution/1 (GET) (No AUTH) => 401', () => {
+    return request(app.getHttpServer())
+      .get('/institution/1')
+      .expect(401);
+  })
+
+  it('/institution/1 (GET) (Worker 1) => 200', async () => {
+    const token = await request(app.getHttpServer())
+      .post('/auth/login/1')
+      .expect(201)
+      .then((res) => res.body.access_token);
+
+    return request(app.getHttpServer())
+      .get('/institution/1')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200);
+  })
+
+  it('/institution/2 (GET) (worker 2) => 403', async () => {
+    const token = await request(app.getHttpServer())
+      .post('/auth/login/1')
+      .expect(201)
+      .then((res) => res.body.access_token);
+
+    return request(app.getHttpServer())
+      .get('/institution/2')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(403);
+  })
+
+  it('/institution/1/workers (GET) (worker 1) => 403', async () => {
+    const token = await request(app.getHttpServer())
+      .post('/auth/login/1')
+      .expect(201)
+      .then((res) => res.body.access_token);
+
+    return request(app.getHttpServer())
+      .get('/institution/1/workers')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(403);
+  })
+
+  it('/institution/1/workers (GET) (admin 5) => 200', async () => {
+    const token = await request(app.getHttpServer())
+      .post('/auth/login/5')
+      .expect(201)
+      .then((res) => res.body.access_token);
+
+    return request(app.getHttpServer())
+      .get('/institution/1/workers')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200);
+  })
+
+  it('/institution/1/worker/1 (GET) (admin 5) => 200', async () => {
+    const token = await request(app.getHttpServer())
+      .post('/auth/login/5')
+      .expect(201)
+      .then((res) => res.body.access_token);
+
+    return request(app.getHttpServer())
+      .get('/institution/1/worker/1')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200);
+  })
+
+  it('/institution/1/worker/4 (GET) (admin 5) => 404', async () => {
+    const token = await request(app.getHttpServer())
+      .post('/auth/login/5')
+      .expect(201)
+      .then((res) => res.body.access_token);
+
+    return request(app.getHttpServer())
+      .get('/institution/1/worker/4')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(404);
+  })
+
+  it('/worker/1 (GET) (worker 1) => 200', async () => {
+    const token = await request(app.getHttpServer())
+      .post('/auth/login/1')
+      .expect(201)
+      .then((res) => res.body.access_token);
+
+    return request(app.getHttpServer())
+      .get('/worker/1')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200);
+  })
+
+
+  it('/worker/1 (GET) (admin 5) => 200', async () => {
+    const token = await request(app.getHttpServer())
+      .post('/auth/login/5')
+      .expect(201)
+      .then((res) => res.body.access_token);
+
+    return request(app.getHttpServer())
+      .get('/worker/1')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200);
+  })
 });
